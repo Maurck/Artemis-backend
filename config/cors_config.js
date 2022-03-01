279dc81a9b15322d7b90bc4b138fa2ca8a45fb97
Maurk <mauricio.lp@astrumla.com>
2022-03-01 02:34:03 -0500

    fix: fixed cors

--- config/cors_config.js
+++ config/cors_config.js
@@ -3,7 +3,7 @@ var cors = require('cors');
 function cors_config(app)
 {
     app.use(cors({
-        origin: 'https://artemis-art.netlify.app',
+        origin: ['https://artemis-art.netlify.app', 'http://localhost:4200'],
         methods: ['GET', 'PATCH', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
         credentials: true
